25b80b1b5c0e5e2f97a6e1ec62bd5c25c5be7779
researchxxl <244199422+researchxxl@users.noreply.github.com>
2025-12-24 18:25:14 +0100

    Update update.cmd

--- scripts/debug/win/update.cmd
+++ scripts/debug/win/update.cmd
@@ -7,6 +7,7 @@ SET /P PACKAGE_NAME=< "%PROJECT_ROOT%\scripts\debug\package_id.txt"
 SET "APK_FULLFN=%PROJECT_ROOT%\app\build\outputs\apk\debug\app-debug.apk"
 ::
 dir "%APK_FULLFN%"
+IF NOT EXIST "%APK_FULLFN%" pause
 adb install -r "%APK_FULLFN%"
 ::
 adb shell pm grant "%PACKAGE_NAME%" android.permission.CAMERA
