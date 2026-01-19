ad7459e0c9d193992735d536a053ef74f313c0dd
Jonas <244199422+researchxxl@users.noreply.github.com>
2026-01-19 15:43:22 +0100

    build(deps): bump android-gradle-plugin from 8.13.2 to 9.0.0 (#189)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.aboutLibraries)
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
 }
 
--- syncthing-repository-android/build.gradle.kts
+++ syncthing-repository-android/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
 }
 
@@ -20,7 +19,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
